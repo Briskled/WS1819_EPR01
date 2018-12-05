90a91779b34b72c97280af22998b52188d76b2fe
Briskled <fex-seven@web.de>
2018-12-05 11:46:31 +0100

    Funktionsbeschreibung und Aufgaben für 05.12.2018

---
+++ Förderkurs/P 7.2 - 05.12.2018/Aufgaben.py
@@ -0,0 +1,39 @@
+# Bei diesen Aufgaben werden Sets behandelt
+# Außerdem wird geübt, anhand einer Beschreibung, eine Funktion zu bauen
+
+
+# Aufgabe 1
+# Schreibe das "Grundgerüst" eines Moduls
+# Dazu gehhört:
+#    - Main-Funktion
+#    - Header
+#    - Aufruf der Main-Funktion
+#    - Docstring des Moduls
+#    - Docstring der Main-Funktion
+
+# Aufgabe 2
+# Schreibe eine Funtion, die einen string A erwartet
+# Sie soll "pong" als zurückgeben, wenn A "ping" ist
+# Ansonsten soll sie None zurückgeben
+
+# Aufgabe 3
+# Schreibe eine Funktion, die ein Set S erwartet.
+# Sie soll die Funktion aus Aufgabe 2 für jedes Element in S aufrufen
+# und den zurückgegebenen Wert mit print ausgeben
+
+# Aufgabe 4
+# Lasse die Main-Funktion die Funktion aus Aufgabe 3 aufrufen
+# Benutze als Argument das Set {"String", "Ping", "Strong", "Pong", "string", "ping"}
+
+# Aufgabe 5
+# Schreibe eine Funktion, die den Benutzer genau 5 Strings eingeben lässt
+
+# Aufgabe 6
+# Modifiziere die Funktion aus Aufgabe 5 so, dass jeder eingegebene String
+# zu einem Set hinzugefügt wird, sodass nach den Eingaben das Set genau 5 Werte hat.
+# Die Funktion soll dieses Set zurückgeben
+
+# Aufgabe 7
+# Lasse die Main-Methode zuerst die Funktion aus Aufgabe 5/6 aufrufen
+# Anschließend soll die Funktion aus Aufgabe 3 mit dessen Ergebnis
+# aufgerufen werden.
