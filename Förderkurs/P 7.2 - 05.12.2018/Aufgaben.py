052224f08593d15777f92c2217cb96fe5e00341b
Briskled <fex-seven@web.de>
2018-12-05 14:00:12 +0100

    FK 05.12.2018

--- Förderkurs/P 7.2 - 05.12.2018/Aufgaben.py
+++ Förderkurs/P 7.2 - 05.12.2018/Aufgaben.py
@@ -1,3 +1,16 @@
+"""Docstring: Irgend eine Beschreibung"""
+
+__author__ = "123456: Felix Lapp"
+__credits__ = ""
+__email__ = ""
+
+def main():
+     """Docstring: Main-Funktion"""
+     pass
+
+
+
+
 # Bei diesen Aufgaben werden Sets behandelt
 # Außerdem wird geübt, anhand einer Beschreibung, eine Funktion zu bauen
 
@@ -12,14 +25,27 @@
 #    - Docstring der Main-Funktion
 
 # Aufgabe 2
-# Schreibe eine Funtion, die einen string A erwartet
+# Schreibe eine Funktion, die einen string A erwartet
 # Sie soll "pong" als zurückgeben, wenn A "ping" ist
 # Ansonsten soll sie None zurückgeben
+
+def task_2(A):
+     if A == "ping":
+          return "pong"
+     else:
+          return None
 
 # Aufgabe 3
 # Schreibe eine Funktion, die ein Set S erwartet.
 # Sie soll die Funktion aus Aufgabe 2 für jedes Element in S aufrufen
 # und den zurückgegebenen Wert mit print ausgeben
+
+
+def task_3(S):
+     for element in S:
+          print(task_2(element))
+
+
 
 # Aufgabe 4
 # Lasse die Main-Funktion die Funktion aus Aufgabe 3 aufrufen
@@ -37,3 +63,8 @@
 # Lasse die Main-Methode zuerst die Funktion aus Aufgabe 5/6 aufrufen
 # Anschließend soll die Funktion aus Aufgabe 3 mit dessen Ergebnis
 # aufgerufen werden.
+
+
+
+if __name__ == "__main__":
+     main()
---
+++ Förderkurs/P 7.2 - 05.12.2018/Standardwerte.py
@@ -0,0 +1,8 @@
+
+b = 25
+def ask(repeat = b, message="Bitte gib etwas ein >>> "):
+     for i in range(repeat):
+          input(message)
+
+
+b = 1
