9b826cf0ab9ef48b827c8b982d84d4914ec8baae
Briskled <fex-seven@web.de>
2018-12-06 11:12:34 +0100

    EPR01 06.12.2018

---
+++ EPR01/06.12.2018/dictionaries.py
@@ -0,0 +1,30 @@
+robot1 = {"Name": "Magnetron", "Größe": 3.5, "Kampfwert": 11 }
+robot2 = {"Name": "abc", "Größe": 3.5, "Kampfwert": 7 }
+robot3 = {"Name": "Saboteur", "Größe": 6, "Kampfwert": 16 }
+robot4 = {"Name": "Robostampf", "Größe": 16, "Kampfwert": 9000 }
+robot5 = {"Name": "Staubsauger 300", "Größe": 0.3, "Kampfwert": 1 }
+
+
+def f(a):
+     a.append(1)
+
+
+# Eine Liste
+l = [1,2,3,4]
+
+
+# kopiert die liste und speichert die Kopie in a
+a = l.copy()
+
+# l  bleibt dadurch unverändert
+a.append(6)
+
+
+
+#immutable datentypen:
+# int
+# float
+# bool
+# str
+# tuple
+# frozenset
