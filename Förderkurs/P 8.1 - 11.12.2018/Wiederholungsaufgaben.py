6b842ea6ebbdd1490768412515f965f695f1e074
Briskled <fex-seven@web.de>
2018-12-11 10:23:26 +0100

    Aufgaben 11.12.2018

---
+++ Förderkurs/P 8.1 - 11.12.2018/Wiederholungsaufgaben.py
@@ -0,0 +1,30 @@
+# Aufgabe 0 (zusammen):
+# Welche Datenstrukturen kennst du? Und was sind überhaupt Datenstrukturen?
+
+# Aufgabe 1:
+# Erstelle eine Liste mit dem Namen test aus Zahlen. Sie sollte mindestens 10 Elemente enthalten
+
+# Aufgane 2:
+# Iteriere mit einer for-Schleife über test und gib mit print jedes Element aus
+
+# Aufgabe 3:
+# Schreibe eine Funktion add, die zwei Argumente erwartet und die Summe beider Argumente
+# zurückgibt
+
+# Aufgabe 4:
+# Schreibe ein Programm, welches den Benutzer zwei Zahlen eingeben lässt
+# Benutze dafür zwei input-Anweisungen
+# Anschließend sollen diese Zahlen mit der add-Funktion aus Aufgabe zwei addiert werden
+# Lass das Ergebnis mit print ausgeben
+
+# Aufgabe 5:
+# Schreibe ein Programm, welches den Benutzer einen Tiernamen eingeben lässt.
+# Es soll zu jedem eingegebenen Tier dessen "Klasse" ausgeben.
+# Beispiele:
+# Benutzer sagt "Schwein". Programm antwortet "Säugetier"
+# Benutzer sagt "Barsch". Programm antwortet "Fisch"
+# Welche Datenstruktur verwendest du?
+
+# Aufgabe 6:
+# Wie prüfst du, ob die Klasse eines eingegebenen Tieres überhaupt bekannt ist?
+# Lasse "Nicht bekannt" ausgeben, wenn das eingegeben Tier nicht bekannt ist.
