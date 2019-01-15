c62727a7256ef2c2e228ef6e801b954fb51ee8db
Briskled <fex-seven@web.de>
2019-01-15 10:21:09 +0100

    Aufgaben FK 15.01.2019

---
+++ Förderkurs/P10.1 - 15.01.2019/Aufgaben.py
@@ -0,0 +1,49 @@
+# Wiederholungsfragen:
+
+# Was ist eine Klasse? Was ein Objekt?
+# Wozu verwendet man Attribute?
+# Was ist der Unterschied zwischen Funktion und Methode?
+# Schreibe eine Klasse >Animal< mit den Attributen >name< und >type<
+
+
+# Aufgabe 1:
+# Schreibe eine Funktion sum_firs_n(n), die die Summe der ersten n ganzen Zahlen
+# berechnet und zurückgibt
+# Wenn n=6 ist, soll sie also 1+2+3+4+5+6 berechnen
+
+
+# Aufgabe 2 (vom Tutor):
+# Schreibe eine REKURSIVE Funktion, die das gleiche tut wie sum_first_n(n)
+
+
+# Aufgabe 3:
+# Schreibe eine rekursive Funktion, die n! (Fakultät) berechnet.
+# Wenn n=6 ist, soll sie also 1*2*3*4*5*6 berechnen
+
+
+# Aufgabe 4 (vom Tutor):
+# gegeben sei folgende rekursive Gleichung:
+#   x(0) = 0
+#   x(n) = 2*x(n-1) + 1
+# Schreibe eine rekursive Funktion, die diese Gleichung implementiert
+
+
+# Aufgabe 5:
+# gegeben sei folgende rekursive Gleichung:
+#   x(0) = 6
+#   x(n) = x(n-1) + 3*x(n-1)
+# Schreibe eine rekursive Funktion, die diese Gleichung implementiert
+
+
+# Aufgabe 6:
+# Die Berechnung für die n-te Fibonacci Zahl sieht so aus:
+#   x(1) = 1
+#   x(2) = 1
+#   x(n) = x(n-1) + x(n-2)
+# Schreibe eine rekursive Funktion, die diese Gleichung implementiert
+
+
+# Aufgabe 7:
+# Berechne mit deinem Programm die 4-te, 5-te, 10., 20. 30., 35. und 40. Fibonaccizahl.
+# Was fällt dir auf?
+# Warum ist das so?
